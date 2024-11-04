05c4314942c2d7a6f70b3c6f4e3558a25770c1cc
Luan Vinícius <luanviniciuzz@gmail.com>
2024-11-03 22:31:41 -0300

    feat: esboço models

--- sistema/models.py
+++ sistema/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# class Empresa(models.Model):
+#     nome = models.CharField(max_length=255)
+#     endereco = models.CharField(max_length=255, blank=False)
+#     telefone = models.CharField(max_length=14, blank=False)
+
+#     def __str__(self):
+#         return self.nome
+
+# class Funcionario(models.Model):
+#     nome = models.CharField(max_length=255, blank=False)
+#     email = models.EmailField(max_length=255, blank=False, unique=True)
+#     empresa = models.ForeignKey(Empresa , blank=False, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.nome
+
+# class Ponto(models.Model):
+#     funcionario = models.ForeignKey(Funcionario, blank=False, on_delete=models.CASCADE)
+#     entrada = models.DateTimeField(blank=False)
+#     saida = models.DateTimeField(blank=False)
+
+#     def __str__(self):
+#         return self.entrada
